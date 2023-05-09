3b35443636828dcc6927ddb16b44a037d4736d5d
Andy Peters <peters.andrew.j@gmail.com>
2023-05-09 17:53:34 +0100

    update

--- analysis_sandbox/sandbox.m
+++ analysis_sandbox/sandbox.m
@@ -34,6 +34,110 @@ colormap(AP_colormap('PWG'));
 clim(prctile(abs(aligned_px_avg(:)),99.5).*[-1,1]);
 axis image;
 
+%% Testing MCMS API
+
+% MCMS API documentation is here: 
+% https://oxford-uat.colonymanagement.org/api/swagger-ui/index.html
+
+% Get authentication token 
+
+basicUrl = 'https://oxford-uat.colonymanagement.org/api';
+authenticateEndpoint = [basicUrl '/authenticate'];
+
+usr = 'ap7';
+psw = 'Bluecookiejar';
+
+headers = struct;
+headers.Accept = '*/*';
+headers.username = usr;
+headers.password = psw;
+
+header_cell = [fieldnames(headers),struct2cell(headers)];
+
+options = weboptions( ...
+    'MediaType','application/json', ...
+    'ContentType','json', ...
+    'RequestMethod','post', ...
+    'HeaderFields',header_cell);
+mcms_token = webread(authenticateEndpoint,options);
+
+% Get procedure list
+
+proceduresEndpoint = [basicUrl '/procedures'];
+headers = struct;
+headers.Accept = 'application/json';
+headers.Authorization = ['Bearer ' mcms_token.token];
+
+header_cell = [fieldnames(headers),struct2cell(headers)];
+
+options = weboptions( ...
+    'MediaType','application/json', ...
+    'ContentType','json', ...
+    'HeaderFields',header_cell);
+
+data = webread(proceduresEndpoint,options);
+
+% Get weights
+
+curr_animal = '02150140';
+
+endpoint = [basicUrl '/animalweights/animal/' curr_animal];
+headers = struct;
+headers.Accept = 'application/json';
+headers.Authorization = ['Bearer ' mcms_token.token];
+
+header_cell = [fieldnames(headers),struct2cell(headers)];
+
+options = weboptions( ...
+    'MediaType','application/json', ...
+    'ContentType','json', ...
+    'HeaderFields',header_cell);
+
+data = webread(endpoint,options);
+
+
+data_timestamps = datetime({data.sampleDate},'InputFormat','yyyy-MM-dd''T''HH:mm:ss.SSSZ','TimeZone','local');
+
+[~,sort_idx] = sort(data_timestamps);
+[data(sort_idx).weightValue]
+
+
+% Get name
+
+curr_animal = 'TOAA1.1a';
+endpoint = [basicUrl '/animals/name/' curr_animal];
+headers = struct;
+headers.Accept = 'application/json';
+headers.Authorization = ['Bearer ' mcms_token.token];
+
+header_cell = [fieldnames(headers),struct2cell(headers)];
+
+options = weboptions( ...
+    'MediaType','application/json', ...
+    'ContentType','json', ...
+    'HeaderFields',header_cell);
+
+data = webread(endpoint,options);
+
+
+% Get project licenses
+
+endpoint = [basicUrl '/projectlicenses'];
+headers = struct;
+headers.Accept = 'application/json';
+headers.Authorization = ['Bearer ' mcms_token.token];
+
+header_cell = [fieldnames(headers),struct2cell(headers)];
+
+options = weboptions( ...
+    'MediaType','application/json', ...
+    'ContentType','json', ...
+    'HeaderFields',header_cell);
+
+data = webread(endpoint,options);
+
+
+
 
 
 
