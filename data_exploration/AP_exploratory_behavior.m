a31c7ab60620c49fc149122a1714fa0dc28b9c6c
Andy Peters <peters.andrew.j@gmail.com>
2024-04-08 18:53:36 +0100

    Update AP_exploratory_behavior.m

--- data_exploration/AP_exploratory_behavior.m
+++ data_exploration/AP_exploratory_behavior.m
@@ -171,7 +171,7 @@ plot(-[trial_events.values(sort_idx).TrialQuiescence],1:length(trial_events.valu
 
 %% Behavior across days
 
-animals = {'AM021','AM022'};
+animals = {'AM021'};
 
 % Create master tiled layout
 figure;
