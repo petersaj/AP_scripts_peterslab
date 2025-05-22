286aae4e965ecf2b8996e143fa539d0050fc0db0
Andy Peters <peters.andrew.j@gmail.com>
2025-05-22 15:57:38 +0100

    update

--- projects/long_striatum/paper/AP_longstriatum_figs_v1.m
+++ projects/long_striatum/paper/AP_longstriatum_figs_v1.m
@@ -3,188 +3,7 @@
 % Figures for longitudinal striatum+cortex project
 
 % Data packaged by AM save scripts
-% Analysis taken fom AP_longstriautm_exploratory_2
-
-%% ------- LOAD/PREP DATA ------------------
-
-%% Behavior
-
-% Set stat and p-value to define learning day
-use_stat = 'firstmove_mean';
-learn_stat_p = 0.05;
-
-% Load behavior
-data_path = fullfile(plab.locations.server_path,'Users','Andrada-Maria_Marica','long_str_ctx_data');
-load(fullfile(data_path,'swr_bhv_v2'));
-
-% Set (overwrite) "learned_days" and "days_from_learning"
-bhv.learned_days = cellfun(@(x) x < learn_stat_p,bhv.(['stimwheel_pval_',use_stat]));
-for curr_animal = unique(bhv.animal)'
-    curr_rows = strcmp(bhv.animal,curr_animal);
-    bhv.days_from_learning(curr_rows) = ...
-        (1:sum(curr_rows))' - ...
-        max([NaN,find(bhv.learned_days(curr_rows),1)]);
-end
-
-
-%% K-means on maps 
-
-% Load maps
-data_path = fullfile(plab.locations.server_path,'Users','Andrada-Maria_Marica','long_str_ctx_data');
-load(fullfile(data_path,'ctx_maps_to_str'));
-U_size = size(all_ctx_maps_to_str.cortex_kernel_px{1},[1,2]);
-
-% K-means cluster maps
-n_k = 3;
-
-% (kmeans starting as maps averaged by depth)
-kmeans_starting = nanmean(cell2mat(permute(cellfun(@(x) ap.groupfun(@mean,x,[],[],ap.quantile_bin(size(x,3),n_k)), ...
-    all_ctx_maps_to_str.cortex_kernel_px(cellfun(@(x) ...
-    size(x,3) >= n_k,all_ctx_maps_to_str.cortex_kernel_px)),'uni',false),[2,3,4,1])),4);
-
-[kidx,kmeans_centroid_flat,~,kmeans_dist] = kmeans(...
-    reshape(cat(3,all_ctx_maps_to_str.cortex_kernel_px{:}),prod(U_size),[])',n_k, ...
-    'Distance','correlation','start',reshape(kmeans_starting,[],n_k)');
-kmeans_centroid = reshape(kmeans_centroid_flat',size(kmeans_starting,1),size(kmeans_starting,2),[]);
-
-% Package kidx by recording (used for some groupings)
-kidx_rec = mat2cell(kidx,cellfun(@(x) size(x,3).*(size(x,1)>0), ...
-    all_ctx_maps_to_str.cortex_kernel_px));
-
-
-%% Passive widefield + ephys
-
-dataset = 'passive';
-
-% Set data path
-data_path = fullfile(plab.locations.server_path,'Users','Andrada-Maria_Marica','long_str_ctx_data');
-
-% Load ephys
-load(fullfile(data_path,'ephys'));
-
-% Load widefield
-load(fullfile(data_path,'ctx_wf'));
-% (TO DO: change saved field to wf.V_no_move_stim_align>V_stim_align
-wf = renamevars(wf,'V_no_move_stim_align','V_stim_align');
-
-% Load master U
-U_master = plab.wf.load_master_U;
-
-
-%% Task widefield + ephys
-
-dataset = 'task';
-
-% Set data path
-data_path = fullfile(plab.locations.server_path,'Users','Andrada-Maria_Marica','long_str_ctx_data');
-
-% Load ephys
-load(fullfile(data_path,'task_ephys_outcome'));
-% (TO DO: change this saved variable to be 'ephys', not 'task_ephys'
-ephys = task_ephys;
-clear task_ephys;
-
-% Load widefield
-load(fullfile(data_path,'task_ctx_wf'));
-
-% Load master U
-U_master = plab.wf.load_master_U;
-
-
-%% Striatum: create cluster MUA and data indices
-
-% Sum spikes from depth into cluster multiunit
-[striatum_psth_sum,striatum_kidx] = cellfun(@(mua,kidx) ...
-    ap.groupfun(@sum,mua,[],[],kidx), ...
-    ephys.binned_msn_spikes_stim_align,kidx_rec,'uni',false);
-
-% Normalize and smooth multiunit
-% (currently psth time is hard coded: save this somewhere)
-psth_t = -0.5:0.001:1;
-baseline_t = psth_t < 0;
-softnorm = 10;
-psth_baseline = cellfun(@(mua) ...
-    mean(mua(:,baseline_t,:),[1,2]) + softnorm, ...
-    striatum_psth_sum,'uni',false,'ErrorHandler',@(varargin) NaN);
-
-psth_norm_smooth_reshape_fcn = @(psth,baseline) ...
-    reshape(permute(smoothdata((psth-baseline)./baseline,2, ...
-    'gaussian',[100,0]),[1,3,2]),[],length(psth_t));
-
-striatum_psth = cell2mat(cellfun(psth_norm_smooth_reshape_fcn, ...
-    striatum_psth_sum,psth_baseline,'uni',false));
-
-% Create grouping indicies for all striatum data
-striatum_psth_grp = struct;
-
-n_trials_rec = cellfun(@(x) size(x,1),striatum_psth_sum);
-n_mua_rec = cellfun(@length,striatum_kidx);
-
-striatum_psth_grp.kidx = cell2mat(cellfun(@(kidx,n_trials) reshape(repmat(kidx',n_trials,1),[],1), ...
-    striatum_kidx,num2cell(n_trials_rec),'uni',false));
-
-striatum_psth_grp.animal = cell2mat(cellfun(@(animal,n_trials,n_mua) repmat(animal,n_trials*n_mua,1), ...
-    num2cell(grp2idx(ephys.animal)),num2cell(n_trials_rec),num2cell(n_mua_rec),'uni',false));
-
-striatum_psth_grp.ld = cell2mat(cellfun(@(animal,n_trials,n_mua) repmat(animal,n_trials*n_mua,1), ...
-    num2cell(bhv.days_from_learning),num2cell(n_trials_rec),num2cell(n_mua_rec),'uni',false));
-
-switch dataset
-    case 'task'
-        striatum_psth_grp.rxn = cell2mat(cellfun(@(rxn,n_mua) reshape(repmat(rxn,1,n_mua),[],1), ...
-            bhv.stim_to_move,num2cell(n_mua_rec),'uni',false));
-    case 'passive'
-        striatum_psth_grp.stim = cell2mat(cellfun(@(stim,n_mua) reshape(repmat(stim,1,n_mua),[],1), ...
-            ephys.trial_stim_values,num2cell(n_mua_rec),'uni',false));
-end
-
-
-
-%% Widefield: create indices
-
-% Grab aligned data time
-wf_t = wf.wf_stim_time{1};
-
-% Create group indicies
-wf_grp = struct;
-
-wf_grp.animal = cell2mat(cellfun(@(animal,data) repmat(animal,size(data,1),1), ...
-    num2cell(grp2idx(wf.animal)),wf.V_stim_align,'uni',false));
-
-wf_grp.ld = cell2mat(cellfun(@(animal,data) repmat(animal,size(data,1),1), ...
-    num2cell(bhv.days_from_learning),wf.V_stim_align,'uni',false));
-
-switch dataset
-    case 'task'
-        wf_grp.rxn = cell2mat(bhv.stim_to_move);
-    case 'passive'
-        wf_grp.stim = cell2mat(wf.trial_stim_values);
-end
-
-
-
-%% Widefield ROIs by corticostriatal maps
-
-% Create ROIs by striatum cluster maps
-kmeans_centroid_blur = imgaussfilt(kmeans_centroid,10);
-[~,m] = max(kmeans_centroid_blur,[],[1,2],'linear');
-[mr,mc] = ind2sub(size(U_master,[1,2]),m);
-striatum_wf_roi = kmeans_centroid_blur > prctile(kmeans_centroid_blur,100,[1,2])*0.50;
-striatum_wf_roi(:,round(size(striatum_wf_roi,2)/2):end,:) = false;
-for k = 1:size(striatum_wf_roi,3)
-    [~,m] = max(imgaussfilt(kmeans_centroid_blur(:,:,k),10).* ...
-        round(linspace(1,0,size(kmeans_centroid_blur,2))),[],[1,2],'linear');
-    [mx,my] = ind2sub(size(U_master,[1,2]),m);
-    striatum_wf_roi(:,:,k) = bwselect(striatum_wf_roi(:,:,k),my,mx);
-end
-
-% Get ROI activity
-wf_striatum_roi = permute(ap.wf_roi(U_master, ...
-    permute(cell2mat(wf.V_stim_align),[3,2,1]),[],[],striatum_wf_roi),[3,2,1]);
-
-% (baseline-subtract)
-baseline_t = wf_t < 0;
-wf_striatum_roi = wf_striatum_roi - nanmean(wf_striatum_roi(:,baseline_t,:),2);
+% Analysis taken fom AP_longstriatum_exploratory_2
 
 
 %% ------- GENERATE FIGURES ----------------
@@ -385,27 +204,27 @@ plot_day_grp = discretize(max(ld_grp,-inf),plot_day_bins);
 n_split = 3;
 split_idx = cell2mat(cellfun(@(n_trials,n_mua) ...
     reshape(repmat(ap.quantile_bin(n_trials,n_split),1,n_mua),[],1), ...
-    num2cell(n_trials_rec),num2cell(n_mua_rec),'uni',false));
+    num2cell(striatum_trials_rec_n),num2cell(striatum_mua_rec_n),'uni',false));
 
-% (mean PSTH, max t, mean across animals)
+% (mean across trials, max in time window, mean across animals)
 use_trials = striatum_psth_grp.rxn > rxn_cutoff;
-[psth_mean,psth_mean_grp] = ap.groupfun(@mean,striatum_psth(use_trials,:), ...
+[activity_mean,activity_mean_grp] = ap.groupfun(@mean,striatum_psth(use_trials,:), ...
     [striatum_psth_grp.animal(use_trials),plot_day_grp(use_trials), ...
     split_idx(use_trials),striatum_psth_grp.kidx(use_trials)]);
 
 max_t = psth_t > 0 & psth_t < 0.2;
-psth_max = max(psth_mean(:,max_t),[],2);
-[psth_max_avg,psth_max_grp] = ap.nestgroupfun({@mean,@mean},psth_max,psth_mean_grp(:,1),psth_mean_grp(:,2:end));
-psth_max_sem = ap.nestgroupfun({@mean,@AP_sem},psth_max,psth_mean_grp(:,1),psth_mean_grp(:,2:end));
+activity_max = max(activity_mean(:,max_t),[],2);
+[activity_max_avg,activity_max_grp] = ap.nestgroupfun({@mean,@mean},activity_max,activity_mean_grp(:,1),activity_mean_grp(:,2:end));
+activity_max_sem = ap.nestgroupfun({@mean,@AP_sem},activity_max,activity_mean_grp(:,1),activity_mean_grp(:,2:end));
 
 figure;
 h = tiledlayout(n_k,max(plot_day_grp),'TileSpacing','compact');
 for curr_k = 1:n_k
     for curr_day = 1:length(plot_day_bins)-1
         nexttile; hold on;
-        curr_data_idx = psth_max_grp(:,1) == curr_day & ...
-            psth_max_grp(:,3) == curr_k;
-        errorbar(psth_max_avg(curr_data_idx),psth_max_sem(curr_data_idx),'k','linewidth',2);
+        curr_data_idx = activity_max_grp(:,1) == curr_day & ...
+            activity_max_grp(:,3) == curr_k;
+        errorbar(activity_max_avg(curr_data_idx),activity_max_sem(curr_data_idx),'k','linewidth',2);
         axis padded
         if curr_k == 1
             title(sprintf('%d:%d',plot_day_bins(curr_day),plot_day_bins(curr_day+1)));
@@ -492,7 +311,43 @@ ap.prettyfig;
 
 %% [Fig 2X] Cortex task max stim activity split aross session
 
+rxn_cutoff = 0.3; % only plot trials with slow reaction times
 
+plot_day_bins = [-Inf,-2:2,Inf];
+plot_day_grp = discretize(max(wf_grp.ld,-inf),plot_day_bins);
+
+% Split by trial percentile within day
+n_split = 3;
+split_idx = cell2mat(cellfun(@(n_trials) ...
+    ap.quantile_bin(n_trials,n_split), ...
+    num2cell(cortex_trials_rec_n),'uni',false));
+
+% (mean across trials, max in time window, mean across animals)
+use_trials = wf_grp.rxn > rxn_cutoff;
+[activity_mean,activity_mean_grp] = ap.groupfun(@mean,wf_striatum_roi(use_trials,:,:), ...
+    [wf_grp.animal(use_trials),plot_day_grp(use_trials),split_idx(use_trials)]);
+
+max_t = wf_t > 0 & wf_t < 0.2;
+activity_max = permute(max(activity_mean(:,max_t,:),[],2),[1,3,2]);
+[activity_max_avg,activity_max_grp] = ap.nestgroupfun({@mean,@mean},activity_max,activity_mean_grp(:,1),activity_mean_grp(:,2:end));
+activity_max_sem = ap.nestgroupfun({@mean,@AP_sem},activity_max,activity_mean_grp(:,1),activity_mean_grp(:,2:end));
+
+figure;
+h = tiledlayout(n_k,max(plot_day_grp),'TileSpacing','compact');
+for curr_k = 1:n_k
+    for curr_day = 1:length(plot_day_bins)-1
+        nexttile; hold on;
+        curr_data_idx = activity_max_grp(:,1) == curr_day;
+        ap.errorfill(1:n_split,activity_max_avg(curr_data_idx,curr_k), ...
+            activity_max_sem(curr_data_idx),[0,0.6,0],0.5,false,2);
+        axis padded
+        if curr_k == 1
+            title(sprintf('%d:%d',plot_day_bins(curr_day),plot_day_bins(curr_day+1)));
+        end
+    end
+end
+linkaxes(h.Children,'xy');
+ap.prettyfig;
 
 
 %% [Fig 3X] Striatum PSTH passive
---
+++ projects/long_striatum/paper/AP_longstriatum_load_data.m
@@ -0,0 +1,210 @@
+% Load data for longitudinal striatum+cortex project
+%
+% Required variable: 
+% load_dataset - dataset to load ('task' or 'passive');
+% 
+% Optional: 
+% load_dataset_overwrite - overwrite if data already loaded (true/false)
+
+%% Check options
+
+% Check for valid dataset selection
+if ~exist('load_dataset','var')
+    error('No load_dataset specified')
+elseif ~ismember(load_dataset,{'passive','task'})
+    error('Unknown load_dataset: %s',load_dataset);
+end
+
+% If no overwrite flag set, turn off by default
+if ~exist('load_dataset_overwrite')
+
+% skip if loaded and no overwrite flag
+
+if exist('loaded_dataset','var') && strcmp(loaded_dataset,load_dataset)
+
+end
+
+return
+
+
+
+%% Behavior
+
+% Set stat and p-value to define learning day
+use_stat = 'firstmove_mean';
+learn_stat_p = 0.05;
+
+% Load behavior
+data_path = fullfile(plab.locations.server_path,'Users','Andrada-Maria_Marica','long_str_ctx_data');
+load(fullfile(data_path,'swr_bhv_v2'));
+
+% Set (overwrite) "learned_days" and "days_from_learning"
+bhv.learned_days = cellfun(@(x) x < learn_stat_p,bhv.(['stimwheel_pval_',use_stat]));
+for curr_animal = unique(bhv.animal)'
+    curr_rows = strcmp(bhv.animal,curr_animal);
+    bhv.days_from_learning(curr_rows) = ...
+        (1:sum(curr_rows))' - ...
+        max([NaN,find(bhv.learned_days(curr_rows),1)]);
+end
+
+
+%% K-means on maps 
+
+% Load maps
+data_path = fullfile(plab.locations.server_path,'Users','Andrada-Maria_Marica','long_str_ctx_data');
+load(fullfile(data_path,'ctx_maps_to_str'));
+U_size = size(all_ctx_maps_to_str.cortex_kernel_px{1},[1,2]);
+
+% K-means cluster maps
+n_k = 3;
+
+% (kmeans starting as maps averaged by depth)
+kmeans_starting = nanmean(cell2mat(permute(cellfun(@(x) ap.groupfun(@mean,x,[],[],ap.quantile_bin(size(x,3),n_k)), ...
+    all_ctx_maps_to_str.cortex_kernel_px(cellfun(@(x) ...
+    size(x,3) >= n_k,all_ctx_maps_to_str.cortex_kernel_px)),'uni',false),[2,3,4,1])),4);
+
+[kidx,kmeans_centroid_flat,~,kmeans_dist] = kmeans(...
+    reshape(cat(3,all_ctx_maps_to_str.cortex_kernel_px{:}),prod(U_size),[])',n_k, ...
+    'Distance','correlation','start',reshape(kmeans_starting,[],n_k)');
+kmeans_centroid = reshape(kmeans_centroid_flat',size(kmeans_starting,1),size(kmeans_starting,2),[]);
+
+% Package kidx by recording (used for some groupings)
+kidx_rec = mat2cell(kidx,cellfun(@(x) size(x,3).*(size(x,1)>0), ...
+    all_ctx_maps_to_str.cortex_kernel_px));
+
+
+%% Widefield + ephys
+
+% Set data path
+data_path = fullfile(plab.locations.server_path,'Users','Andrada-Maria_Marica','long_str_ctx_data');
+
+% Load master U
+U_master = plab.wf.load_master_U;
+
+switch load_dataset
+
+    case 'passive'
+
+        % Load ephys
+        load(fullfile(data_path,'ephys'));
+
+        % Load widefield
+        load(fullfile(data_path,'ctx_wf'));
+        % (TO DO: change saved field to wf.V_no_move_stim_align>V_stim_align
+        wf = renamevars(wf,'V_no_move_stim_align','V_stim_align');
+
+    case 'task'
+
+        % Load ephys
+        load(fullfile(data_path,'task_ephys_outcome'));
+        % (TO DO: change this saved variable to be 'ephys', not 'task_ephys'
+        ephys = task_ephys;
+        clear task_ephys;
+
+        % Load widefield
+        load(fullfile(data_path,'task_ctx_wf'));
+
+end
+
+
+%% Striatum: create cluster MUA and data indices
+
+% Sum spikes from depth into cluster multiunit
+[striatum_psth_sum,striatum_kidx] = cellfun(@(mua,kidx) ...
+    ap.groupfun(@sum,mua,[],[],kidx), ...
+    ephys.binned_msn_spikes_stim_align,kidx_rec,'uni',false);
+
+% Normalize and smooth multiunit
+% (currently psth time is hard coded: save this somewhere)
+psth_t = -0.5:0.001:1;
+baseline_t = psth_t < 0;
+softnorm = 10;
+psth_baseline = cellfun(@(mua) ...
+    mean(mua(:,baseline_t,:),[1,2]) + softnorm, ...
+    striatum_psth_sum,'uni',false,'ErrorHandler',@(varargin) NaN);
+
+psth_norm_smooth_reshape_fcn = @(psth,baseline) ...
+    reshape(permute(smoothdata((psth-baseline)./baseline,2, ...
+    'gaussian',[100,0]),[1,3,2]),[],length(psth_t));
+
+striatum_psth = cell2mat(cellfun(psth_norm_smooth_reshape_fcn, ...
+    striatum_psth_sum,psth_baseline,'uni',false));
+
+% Create grouping indicies for all striatum data
+striatum_psth_grp = struct;
+
+striatum_trials_rec_n = cellfun(@(x) size(x,1),striatum_psth_sum);
+striatum_mua_rec_n = cellfun(@length,striatum_kidx);
+
+striatum_psth_grp.kidx = cell2mat(cellfun(@(kidx,n_trials) reshape(repmat(kidx',n_trials,1),[],1), ...
+    striatum_kidx,num2cell(striatum_trials_rec_n),'uni',false));
+
+striatum_psth_grp.animal = cell2mat(cellfun(@(animal,n_trials,n_mua) repmat(animal,n_trials*n_mua,1), ...
+    num2cell(grp2idx(ephys.animal)),num2cell(striatum_trials_rec_n),num2cell(striatum_mua_rec_n),'uni',false));
+
+striatum_psth_grp.ld = cell2mat(cellfun(@(animal,n_trials,n_mua) repmat(animal,n_trials*n_mua,1), ...
+    num2cell(bhv.days_from_learning),num2cell(striatum_trials_rec_n),num2cell(striatum_mua_rec_n),'uni',false));
+
+switch load_dataset
+    case 'task'
+        striatum_psth_grp.rxn = cell2mat(cellfun(@(rxn,n_mua) reshape(repmat(rxn,1,n_mua),[],1), ...
+            bhv.stim_to_move,num2cell(striatum_mua_rec_n),'uni',false));
+    case 'passive'
+        striatum_psth_grp.stim = cell2mat(cellfun(@(stim,n_mua) reshape(repmat(stim,1,n_mua),[],1), ...
+            ephys.trial_stim_values,num2cell(striatum_mua_rec_n),'uni',false));
+end
+
+
+
+%% Widefield: create indices
+
+% Grab aligned data time
+wf_t = wf.wf_stim_time{1};
+
+% Create group indicies
+wf_grp = struct;
+
+wf_grp.animal = cell2mat(cellfun(@(animal,data) repmat(animal,size(data,1),1), ...
+    num2cell(grp2idx(wf.animal)),wf.V_stim_align,'uni',false));
+
+wf_grp.ld = cell2mat(cellfun(@(animal,data) repmat(animal,size(data,1),1), ...
+    num2cell(bhv.days_from_learning),wf.V_stim_align,'uni',false));
+
+switch load_dataset
+    case 'task'
+        wf_grp.rxn = cell2mat(bhv.stim_to_move);
+    case 'passive'
+        wf_grp.stim = cell2mat(wf.trial_stim_values);
+end
+
+cortex_trials_rec_n = cellfun(@(x) size(x,1),wf.V_stim_align);
+
+
+%% Widefield ROIs by corticostriatal maps
+
+% Create ROIs by striatum cluster maps
+kmeans_centroid_blur = imgaussfilt(kmeans_centroid,10);
+[~,m] = max(kmeans_centroid_blur,[],[1,2],'linear');
+[mr,mc] = ind2sub(size(U_master,[1,2]),m);
+striatum_wf_roi = kmeans_centroid_blur > prctile(kmeans_centroid_blur,100,[1,2])*0.50;
+striatum_wf_roi(:,round(size(striatum_wf_roi,2)/2):end,:) = false;
+for k = 1:size(striatum_wf_roi,3)
+    [~,m] = max(imgaussfilt(kmeans_centroid_blur(:,:,k),10).* ...
+        round(linspace(1,0,size(kmeans_centroid_blur,2))),[],[1,2],'linear');
+    [mx,my] = ind2sub(size(U_master,[1,2]),m);
+    striatum_wf_roi(:,:,k) = bwselect(striatum_wf_roi(:,:,k),my,mx);
+end
+
+% Get ROI activity
+wf_striatum_roi = permute(ap.wf_roi(U_master, ...
+    permute(cell2mat(wf.V_stim_align),[3,2,1]),[],[],striatum_wf_roi),[3,2,1]);
+
+% (baseline-subtract)
+baseline_t = wf_t < 0;
+wf_striatum_roi = wf_striatum_roi - nanmean(wf_striatum_roi(:,baseline_t,:),2);
+
+
+%% Set flag for loaded data
+
+loaded_dataset = load_dataset;
+
