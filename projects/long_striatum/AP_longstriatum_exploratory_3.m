9396e2f054ba83f1fe28efee753fb6f0c3967d3d
Andy Peters <peters.andrew.j@gmail.com>
2025-06-05 13:27:32 +0100

    update

--- projects/long_striatum/AP_longstriatum_exploratory_3.m
+++ projects/long_striatum/AP_longstriatum_exploratory_3.m
@@ -409,20 +409,47 @@ linkaxes(h.Children,'xy');
 legend({'R-C','R-L'});
 ap.prettyfig;
 
-%% (cleaned above)
-
-% (combining kidx 1+2)
-plot_kidx = 1:2;
+%% (above, v2)
 
 %%% Load data for figure
 load_dataset = 'passive';
 AP_longstriatum_load_data;
 %%%
 
+% Set striatal domains to plot (combines if multiple)
+plot_kidx = 1;
+
+% Set days to group
 plot_day_bins = [-inf,0,inf];
 plot_day_grp = discretize(striatum_sua_grp.ld,plot_day_bins);
 
-stim_t = psth_t > 0.05 & psth_t < 0.15;
+% Get max activity in type-relevant window
+celltypes = ["msn","fsi","tan"];
+striatum_sua_tavg = nan(size(striatum_sua,[1,3]));
+for curr_celltype = celltypes
+
+    curr_units = find( ...
+        ismember(striatum_sua_grp.kidx,plot_kidx) & ...
+        striatum_sua_grp.(curr_celltype));
+
+    % Get window from mean activity across all cells
+    curr_sua_mean = mean(ap.groupfun(@mean, ...
+        striatum_sua(curr_units,:,3),striatum_sua_grp.animal(curr_units)),1);
+
+    [curr_sua_mean_max,curr_sua_mean_max_idx] = max(curr_sua_mean);
+    act_thresh = curr_sua_mean_max*0.2;
+
+    act_window = ...
+        psth_t([find(curr_sua_mean < act_thresh & ...
+        1:length(curr_sua_mean) < curr_sua_mean_max_idx,1,'last'), ...
+        find(curr_sua_mean < act_thresh & ...
+        1:length(curr_sua_mean) > curr_sua_mean_max_idx,1,'first')]);
+
+    stim_t = psth_t > act_window(1) & psth_t < act_window(2);
+
+    % Get max activity within window
+    striatum_sua_tavg(curr_units,:) = mean(striatum_sua(curr_units,stim_t,:),2);
+end
 
 n_stim = size(striatum_sua,3);
 
@@ -440,10 +467,10 @@ for curr_day_grp = 1:length(plot_day_bins)-1
 
     % (sort max stim, then max within stim)
     sort_idx_cell = cell(3,1);
-    [~,max_stim] = max(mean(striatum_sua(curr_units,stim_t,:),2),[],3);
+    [~,max_stim] = max(striatum_sua_tavg,[],2);
     for curr_stim_sort = 1:3
-        curr_stim_units = find(max_stim==curr_stim_sort);
-        [~,curr_sort_idx] = sort(max(mean(striatum_sua(curr_units(curr_stim_units),stim_t,curr_stim_sort),2),[],3),'descend');
+        curr_stim_units = find(max_stim(curr_units)==curr_stim_sort);
+        [~,curr_sort_idx] = sort(max(striatum_sua_tavg(curr_units(curr_stim_units),curr_stim_sort),[],2),'descend');
         sort_idx_cell{curr_stim_sort} = curr_stim_units(curr_sort_idx);
     end
     sort_idx = cell2mat(sort_idx_cell);
@@ -468,28 +495,17 @@ end
 linkaxes(h.Children,'xy');
 ap.prettyfig;
 
+% Plot example PSTHs
+psth_unit_coordinates = { ...
+    'AM026','2024-07-25',354; ... % pre-learn
+    'AM022','2024-04-06',246};    % post-learn
 
-%%%%% TESTING PSTH
-
-stim_cell_borders = cumsum(cellfun(@length,sort_idx_cell))+1;
-
-% i = stim_cell_borders(1);
-i = stim_cell_borders(2)+3;
-
-animal = curr_sorted_unit_coordinate{i,1};
-rec_day = curr_sorted_unit_coordinate{i,2};
-plot_units = curr_sorted_unit_coordinate{i,3};
-AP_longstriatum_psth_fig;
-
-% good ones (top sort+3)
-% pre: 'AM026' '2024-07-25' 354
-% % post: 'AM022' '2024-04-06' 246
-% animal = 'AM022';
-% rec_day = '2024-04-06';
-% plot_units = 246;
-% AP_longstriatum_psth_fig;
-
-%%%%%%%%%
+for curr_unit = 1:size(psth_unit_coordinates,1)
+    animal = psth_unit_coordinates{curr_unit,1};
+    rec_day = psth_unit_coordinates{curr_unit,2};
+    plot_units = psth_unit_coordinates{curr_unit,3};
+    AP_longstriatum_psth_fig;
+end
 
 % Plot average
 figure;
@@ -516,10 +532,10 @@ ap.prettyfig;
 xlim(h.Children(1),[-0.2,0.8]);
 
 % Stim response by celltype
-celltype_order = {'msn','fsi','tan'};
-celltype_id = sum([striatum_sua_grp.(celltype_order{1}), ...
-    striatum_sua_grp.(celltype_order{2}), ...
-    striatum_sua_grp.(celltype_order{3})].*[1,2,3],2);
+plot_celltypes = ["tan","msn"];
+celltype_id = sum(cell2mat(arrayfun(@(x) ...
+    striatum_sua_grp.(plot_celltypes(x)).*x, ...
+    1:length(plot_celltypes),'uni',false)),2);
 
 stim_colormap = ap.colormap('BWR',n_stim);
 
@@ -531,29 +547,27 @@ for curr_day_grp = 1:max(plot_day_grp)
         ismember(striatum_sua_grp.kidx,plot_kidx) & ...
         plot_day_grp == curr_day_grp & celltype_id ~= 0);
 
-    curr_unit_mean = squeeze(mean(striatum_sua(curr_units,stim_t,:),2));
-
-    curr_act_mean = ap.nestgroupfun({@nanmean,@nanmean},curr_unit_mean, ...
+    curr_act_mean = ap.nestgroupfun({@nanmean,@nanmean},striatum_sua_tavg(curr_units,:), ...
         striatum_sua_grp.animal(curr_units),celltype_id(curr_units));
 
-    curr_act_sem = ap.nestgroupfun({@nanmean,@AP_sem},curr_unit_mean, ...
+    curr_act_sem = ap.nestgroupfun({@nanmean,@AP_sem},striatum_sua_tavg(curr_units,:), ...
         striatum_sua_grp.animal(curr_units),celltype_id(curr_units));
 
     nexttile; hold on; set(gca,'ColorOrder',stim_colormap);
-    celltype_x = reordercats(categorical(celltype_order),celltype_order);
+    celltype_x = reordercats(categorical(plot_celltypes),plot_celltypes);
     b = bar(celltype_x,curr_act_mean);
     errorbar(vertcat(b.XEndPoints)',curr_act_mean,curr_act_sem, ...
         'marker','none','linestyle','none','color','k','linewidth',1);
 
     % ~stats~ %
     rc_diff = ap.nestgroupfun({@nanmean,@nanmean}, ...
-        diff(curr_unit_mean(:,[2,3]),[],2), ...
+        diff(striatum_sua_tavg(curr_units,[2,3]),[],2), ...
         striatum_sua_grp.animal(curr_units),celltype_id(curr_units));
 
     n_shuff = 1000;
-    rc_diff_shuff = nan(3,n_shuff);
+    rc_diff_shuff = nan(length(plot_celltypes),n_shuff);
     for curr_shuff = 1:n_shuff
-        data_shuff = ap.shake(curr_unit_mean(:,[2,3]),2);
+        data_shuff = ap.shake(striatum_sua_tavg(curr_units,[2,3]),2);
         rc_diff_shuff(:,curr_shuff) = ...
             ap.nestgroupfun({@nanmean,@nanmean}, ...
             diff(data_shuff,[],2), ...
@@ -561,7 +575,8 @@ for curr_day_grp = 1:max(plot_day_grp)
     end
     stat_rank = tiedrank([rc_diff,rc_diff_shuff]');
     stat_p = 1-stat_rank(1,:)/(n_shuff+1);
-    title(sprintf('RvC p = %.2f/%.2f/%.2f',stat_p));
+    arrayfun(@(x) fprintf('%s daygroup %d RvC: p = %.3g\n', ...
+        plot_celltypes(x),curr_day_grp,stat_p(x)),1:length(plot_celltypes))
 
 end
 linkaxes(h.Children,'xy');
