6a209368c452b51e77bb4b5fd7379fe8f14789cb
Andy Peters <peters.andrew.j@gmail.com>
2026-01-16 11:18:46 +0000

    udate

--- data_exploration/AP_exploratory_behavior.m
+++ data_exploration/AP_exploratory_behavior.m
@@ -13,8 +13,8 @@ quiescent_trials = arrayfun(@(x) ~any(wheel_move(...
 % stim_x = vertcat(trial_events.values.StimFrequence);
 % use_align = stimOn_times(stim_x == 8000  & quiescent_trials);
 
-% stim_x = vertcat(trial_events.values.TrialStimX);
-% use_align = stimOn_times(quiescent_trials & stim_x == 90);
+stim_x = vertcat(trial_events.values.TrialStimX);
+use_align = stimOn_times(quiescent_trials & stim_x == 90);
 
 % stim_y = vertcat(trial_events.values.TrialStimY);
 % use_align = stimOn_times(quiescent_trials & stim_y == 30);
@@ -27,7 +27,7 @@ quiescent_trials = arrayfun(@(x) ~any(wheel_move(...
 
 % use_align = reward_times;
 
-use_align = stimOn_times;
+% use_align = stimOn_times;
 
 % use_align = stimOff_times(trial_opacity == 1);
 
