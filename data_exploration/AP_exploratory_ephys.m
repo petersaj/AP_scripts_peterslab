cfcf20cbbc1887ada974e0dd5610c91e93fc6049
Andy Peters <peters.andrew.j@gmail.com>
2026-09-10 14:46:02 +0100

    Update AP_exploratory_ephys.m

--- data_exploration/AP_exploratory_ephys.m
+++ data_exploration/AP_exploratory_ephys.m
@@ -841,7 +841,7 @@ colormap(AP_colormap('BWR'));
 
 %% Grab and plot histology pictures (pre-SMZ)
 
-animals = {'AP022'};
+animals = {'AP018'};
 
 for curr_animal = 1:length(animals)
     animal = animals{curr_animal};
